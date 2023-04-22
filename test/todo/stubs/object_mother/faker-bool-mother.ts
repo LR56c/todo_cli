812ca2ad64590b52b698400e980d9eb54eb61e48
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-04-22 01:10:48 -0400

    fix(misc): fix imports

--- test/todo/stubs/object_mother/faker-bool-mother.ts
+++ test/todo/stubs/object_mother/faker-bool-mother.ts
@@ -1,7 +1,8 @@
-import {faker} from "@faker-js/faker";
 
 export class FakerBoolMother {
     static random(): boolean {
-        return faker.datatype.boolean()
+        // return faker.datatype.boolean()
+        return true
+
     }
 }
