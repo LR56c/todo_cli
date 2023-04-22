94bbeffdc2fa023664614ef378f3edf02d9fd0b2
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-04-22 00:48:14 -0400

    fix(todo.domain): add from method

--- src/lib/todo/domain/entities/todo.ts
+++ src/lib/todo/domain/entities/todo.ts
@@ -1,26 +1,44 @@
-import { TodoId } from './todo_id';
-import { TodoTitle } from './todo_title';
-import { TodoCompleted } from './todo_completed';
-import { z } from "zod";
+import {TodoId} from './todo_id'
+import {TodoTitle} from './todo_title'
+import {TodoCompleted} from './todo_completed'
+import {z} from "zod"
+import {CreatedAt, UpdatedAt} from "../../../shared"
+import {ValidDate} from "../../../shared/domain/value_objects/valid-date";
 
 export class Todo {
-  private constructor(
-    public readonly todoId: TodoId,
-    public readonly todoTitle: TodoTitle,
-    public readonly todoCompleted: TodoCompleted,
-    public readonly createdAt: Date,
-    public readonly updatedAt: Date,
-  ) {}
+    private constructor(
+        public readonly todoId: TodoId,
+        public readonly todoTitle: TodoTitle,
+        public readonly todoCompleted: TodoCompleted,
+        public readonly createdAt: Date,
+        public readonly updatedAt: Date,
+    ) {
+    }
 
-  public static create(
-    todoId: TodoId,
-    todoTitle: TodoTitle,
-    todoCompleted: TodoCompleted,
-    createdAt: Date,
-    updatedAt: Date,
-  ): Todo {
-    z.date().parse(createdAt);
-    z.date().parse(updatedAt);
-    return new Todo(todoId, todoTitle, todoCompleted, createdAt, updatedAt);
-  }
+    public static create(
+        todoId: TodoId,
+        todoTitle: TodoTitle,
+        todoCompleted: TodoCompleted,
+        createdAt: CreatedAt,
+        updatedAt: UpdatedAt,
+    ): Todo {
+
+        return new Todo(todoId, todoTitle, todoCompleted, createdAt.value, updatedAt.value)
+    }
+
+    public static from(plain: {
+        todoId: string,
+        todoTitle: string,
+        todoCompleted: boolean,
+        createdAt: Date,
+        updatedAt: Date,
+    }): Todo {
+        return new Todo(
+            new TodoId(plain.todoId),
+            new TodoTitle(plain.todoTitle),
+            new TodoCompleted(plain.todoCompleted),
+            new ValidDate(plain.createdAt).value,
+            new ValidDate(plain.updatedAt).value
+        )
+    }
 }
