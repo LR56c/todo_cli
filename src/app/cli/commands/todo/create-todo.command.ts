66ebde65d2acd764cde36530b2f1ef6528995761
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-05-04 22:07:14 -0400

    fix(todo-test-todoCreate-e2e): fix todo command

--- src/app/cli/commands/todo/create-todo.command.ts
+++ src/app/cli/commands/todo/create-todo.command.ts
@@ -10,8 +10,8 @@ import {
   UpdatedAt
 } from "../../../../lib"
 import {v4 as uuid} from "uuid"
-import {z} from "zod";
-import {Command, CommandRunner} from "nest-commander";
+import {z} from "zod"
+import {Command, CommandRunner} from "nest-commander"
 
 @Command({
   name: 'create',
@@ -24,12 +24,12 @@ export class CreateTodoCommand extends CommandRunner {
   }
 
   async run(inputs: string[], options: Record<string, any>): Promise<void> {
-    // recibir parametros y validar tipo solamente
     try {
-      this.ensureParams(inputs[0])
+      const title = this.ensureParams(inputs[0])
+
       const result = await this.todoCreator.execute(
         new TodoId(uuid()),
-        new TodoTitle('title'),
+        new TodoTitle(title),
         new TodoCompleted(false),
         new CreatedAt(new Date()),
         new UpdatedAt(new Date(), new Date()))
@@ -37,26 +37,11 @@ export class CreateTodoCommand extends CommandRunner {
       result.unwrap()
       ProcessStatusMiddleware(StatusCodes.OK)
     } catch (e) {
-      // console.log(e)
       ProcessStatusMiddleware(StatusCodes.BAD_REQUEST)
     }
-
-    // Metodo 1: lanzar process code
-    // await new ProcessStatusMiddleware().handle(StatusCodes.OK)
-
-    // Metodo 2: lanzar cmd
-    // pero sin colocar console.log como accion final o reaccion a evento, dentro del comando, ya que bota test
-    // spawn(inputs[0], {
-    //   shell: true
-    // })
-
-    // Metodo extra: lanzar excepciones, pero al hacerlo, involucra meter dominio especifico
-    // Opciones:
-    // - lanzar excepciones en comando y transformarlos a process code
-    // - recibir de either excepcion de dominio y transformarlos a process code
   }
 
-  private ensureParams(title: string): void {
-    z.string().parse(title)
+  private ensureParams(title: string): string {
+    return z.string().parse(title)
   }
 }
--- test/todo/e2e/create_todo.command.spec.ts
+++ test/todo/e2e/create_todo.command.spec.ts
@@ -1,53 +1,67 @@
-import {Test, TestingModule} from '@nestjs/testing';
-import {AppModule, TodoCreator, TodoService} from "../../../src";
-import {TodoRepositoryMock} from "../integration";
-import {CommandTestFactory} from "nest-commander-testing";
+import {Test, TestingModule} from '@nestjs/testing'
+import {AppModule, TodoCreator, TodoService} from "../../../src"
+import {TodoRepositoryMock} from "../integration"
+import {CommandTestFactory} from "nest-commander-testing"
+import {TodoMother, TodoTitleMother} from "../stubs"
 
 describe('Create command', () => {
-  let commandInstance: TestingModule;
+  let commandInstance: TestingModule
 
   let todoRepositoryMock: TodoRepositoryMock
   let todoCreator: TodoCreator
 
   beforeEach(async () => {
-    jest.clearAllMocks();
-    todoRepositoryMock = new TodoRepositoryMock([]);
+    jest.clearAllMocks()
+    todoRepositoryMock = new TodoRepositoryMock([])
 
     commandInstance = await CommandTestFactory.createTestingCommand({
       imports: [AppModule]
     })
       .overrideProvider(TodoService)
       .useValue(todoRepositoryMock)
-      .compile();
+      .compile()
 
     todoCreator = await commandInstance
       .resolve(TodoCreator)
-  });
+  })
 
-  it('should create todo', async () => {
+  it('should exit success when create todo if input is valid', async () => {
     // Arrange
     const processExit = jest
       .spyOn(process, 'exit')
-      .mockImplementation((code?: number) => undefined as never);
+      .mockImplementation((code?: number) => undefined as never)
 
     const todoCreatorMock = jest
       .spyOn(todoCreator, 'execute')
 
     // Act
-    await CommandTestFactory.run(commandInstance, ['create', 'foo']);
+    await CommandTestFactory.run(commandInstance, ['create', 'f1234'])
 
     // Assert
-    expect(processExit).toHaveBeenCalledWith(0);
-    expect(todoRepositoryMock.saveMock).toHaveBeenCalledTimes(1);
-    expect(todoCreatorMock).toHaveBeenCalledTimes(1);
+    expect(processExit).toHaveBeenCalledWith(0)
+    expect(todoRepositoryMock.saveMock).toHaveBeenCalledTimes(1)
+    expect(todoCreatorMock).toHaveBeenCalledTimes(1)
     expect(processExit).toHaveBeenCalledTimes(1)
-    processExit.mockRestore();
+    processExit.mockRestore()
+  })
 
-    // const spawnSpy = jest.spyOn(childProcess, 'spawn');
-    // expect(spawnSpy).toBeCalledWith('echo Hello World!', { shell: true });
+  it('should fail status when input is invalid', async () => {
+    // Arrange
+    const processExit = jest
+      .spyOn(process, 'exit')
+      .mockImplementation((code?: number) => undefined as never)
 
-    // const exitSpy = jest.spyOn(process, 'exit');
-    // expect(exitSpy).toBeCalledWith(201, { shell: true });
-    // expect(exitSpy).toBeCalledTimes(1)
-  });
-});
+    const todoCreatorMock = jest
+      .spyOn(todoCreator, 'execute')
+
+    // Act
+    await CommandTestFactory.run(commandInstance, ['create', 'a'])
+
+    // Assert
+    expect(processExit).toHaveBeenCalledWith(5)
+    expect(todoRepositoryMock.saveMock).toHaveBeenCalledTimes(0)
+    expect(todoCreatorMock).toHaveBeenCalledTimes(0)
+    expect(processExit).toHaveBeenCalledTimes(1)
+    processExit.mockRestore()
+  })
+})
