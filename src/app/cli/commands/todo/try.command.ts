6007b08ea79d2c2f80970b48a615a729054a3bee
Mauricio Herrera Ortega <ronalli56@gmail.com>
2023-05-18 01:41:29 -0400

    fix(todo-inmemory-criteria-impl): continue fix where inmemory converter

--- src/app/cli/commands/todo/try.command.ts
+++ src/app/cli/commands/todo/try.command.ts
@@ -1,16 +1,17 @@
 import {Command, CommandRunner} from "nest-commander"
-import {Order, OrderBy, OrderType, OrderTypes, TodoFinder} from "../../../../lib"
-
-interface Person {
-  id: number
-  name: string
-  age: number
-}
-
-interface TodoTry {
-  id: string
-  title: string
-}
+import {
+  CreatedAt,
+  Criteria,
+  Filter,
+  FilterField,
+  FilterOperator,
+  FilterValue,
+  Order,
+  Todo, TodoCompleted,
+  TodoFinder,
+  TodoId, TodoTitle, UpdatedAt
+} from "../../../../lib"
+import {TodoMother} from "../../../../../test";
 
 @Command({
   name: 'try',
@@ -36,108 +37,88 @@ export class TryCommand extends CommandRunner {
       //     new CreatedAt(new Date()),
       //     new UpdatedAt(new Date(), new Date()))
 
-      // const criteria = new Criteria([
-      //   new Filter(
-      //     new FilterField('id'),
-      //     new FilterOperator('equals'),
-      //     new FilterValue('01H0EECR22XTH4FKD8BM831JAD')
-      //   )
-      // ])
+      // const criteriaTry = {
+      //   where: {
+      //     id: {
+      //       equals: '01H0EECR22XTH4FKD8BM831JAD'
+      //     },
+      //     completed: {
+      //       equals: false
+      //     },
+      //     title: {
+      //       equals: 'title 1'
+      //     },
+      //     as: {
+      //       equals: 'title 1'
+      //     }
+      //   },
+      //   orderBy: {
+      //     id: 'asc',
+      //     title: 'desc'
+      //   },
+      //   cursor: {
+      //     id: '01H0EECR22XTH4FKD8BM831JAD'
+      //     // id: null
+      //   },
+      //   take: 2,
+      //   skip: null,
+      //   distinct: ['id', 'title']
+      // }
 
-      // const people: Person[] = [
-      //   { id: 1, name: 'John', age: 25 },
-      //   { id: 2, name: 'Jane', age: 30 },
-      //   { id: 3, name: 'John', age: 35 },
-      //   { id: 4, name: 'Mary', age: 40 },
-      //   { id: 5, name: 'John', age: 45 },
-      // ]
-      //
-      // const keys: (keyof Person)[] = ['id', 'name', 'age']
-      // const testTodos = keys.reduce((result, attribute) => this.testList(result, attribute), people)
-
-      const dataList: TodoTry[] = [
-        {
-          id: '01H0EECR22XTH4FKD8BM831JAD',
-          title: '7'
-        },
-        {
-          id: '3',
-          title: '5'
-        },
-        {
-          id: '1',
-          title: '6'
-        },
+      const todos = [
+        Todo.create(
+          new TodoId('01H0PKXF3QE8VMARP58Z5RQC7J'),
+          new TodoTitle('title 1'),
+          new TodoCompleted(false),
+          new CreatedAt(new Date()),
+          new UpdatedAt(new Date(), new Date())
+        ),
+        Todo.create(
+          new TodoId('01H0PKXF3QH9MWQDTQK2MDEMTW'),
+          new TodoTitle('title 2'),
+          new TodoCompleted(false),
+          new CreatedAt(new Date()),
+          new UpdatedAt(new Date(), new Date())
+        ),
+        Todo.create(
+          new TodoId('01H0PKXF3QDMHEQ5J1SD158GRM'),
+          new TodoTitle('title 3'),
+          new TodoCompleted(false),
+          new CreatedAt(new Date()),
+          new UpdatedAt(new Date(), new Date())
+        )
       ]
 
-      const criteria = {
-        where: {
-          id: {
-            equals: '01H0EECR22XTH4FKD8BM831JAD'
-          },
-          // title: {
-          //   equals: 'title 1'
-          // },
-          // as: {
-          //   equals: 'title 1'
-          // }
-        },
-        orderBy: {
-          id: 'asc',
-          title: 'desc'
-        },
-        cursor: {
-          id: '01H0EECR22XTH4FKD8BM831JAD'
-          // id: null
-        },
-        take: 2,
-        skip: null,
-        distinct: ['id', 'title']
-      }
+      const criteria = new Criteria((
+        [
+          new Filter(
+            new FilterField('todoId'),
+            new FilterOperator('equals'),
+            new FilterValue('01H0PKXF3QE8VMARP58Z5RQC7J')
+          ),
+          // new Filter(
+          //   new FilterField('todoCompleted'),
+          //   new FilterOperator('equals'),
+          //   new FilterValue('false')
+          // ),
+          // new Filter(
+          //   new FilterField('todoTitle'),
+          //   new FilterOperator('equals'),
+          //   new FilterValue('title 1')
+          // ),
+          // new Filter(
+          //   new FilterField('as'),
+          //   new FilterOperator('equals'),
+          //   new FilterValue('a')
+          // )
+        ]
+      ))
+      const fil = this.whereList(todos, criteria.filters)
+      console.log('fil')
+      console.log(fil)
 
-      const whereKeys = Object.keys(criteria.where)
-      const dataKeys = Object.keys(dataList[0])
-
-      if (dataKeys.length !== whereKeys.length) {
-        console.log('raro')
-      }
-
-      // reviso cada todo que llega
-
-      const dataFiltered: TodoTry[] = dataList.map((data) => {
-
-        let dataObject = {} as TodoTry
-
-        // aplico el filtro a cada key del todo
-        for (let dataKeyIndex = 0; dataKeyIndex < dataKeys.length; dataKeyIndex++) {
-          const key = dataKeys[dataKeyIndex];
-
-          const elementData = data[key]
-
-          if (key === whereKeys[dataKeyIndex]) {
-
-            const whereElementObject = criteria.where[key]
-            const whereElementKey = Object.keys(whereElementObject)[0]
-            const whereElementData = whereElementObject[whereElementKey]
-
-            if (whereElementKey === 'equals') {
-              if (elementData !== whereElementData) {
-                console.log('not equals')
-                break;
-              }
-            }
-            console.log('filter')
-          }
-
-          dataObject = Object.assign(dataObject, {[key]: elementData})
-        }
-        return dataObject
-      })
-
-      console.log(dataList)
-      console.log(dataFiltered)
-
-      // const distinctedData : TodoTry[] = criteria.distinct.reduce((result, attribute) => this.distinctList(result, attribute), dataList)
+      return
+      // const distinctedData : TodoTry[] = criteriaTry.distinct.reduce((result, attribute) => this.distinctList(result, attribute), dataList)
 
       // const orders = [
       //   new Order(new OrderBy('title'), new OrderType(OrderTypes.ASC)),
@@ -147,38 +128,32 @@ export class TryCommand extends CommandRunner {
 
       // let skipCounter = 0
       // let takeCounter = 0
-
-      // const cursorId = criteria.cursor.id;
-      // const skipValue = criteria.skip;
-      // const takeValue = criteria.take;
-
+      //
+      // const cursorId = criteriaTry.cursor.id;
+      // const skipValue = criteriaTry.skip;
+      // const takeValue = criteriaTry.take;
+      //
       // let canTakeCursor = false
-
+      //
       // let dataLimited: TodoTry[] = []
-      // for (let data of orderedData) {
-
+      // for (let data of dataList) {
+      //
       //   if (cursorId !== null) {
-
+      //
       //     if (cursorId === data.id) {
       //       canTakeCursor = true
       //     }
-
-      //     if (!canTakeCursor) {
-      //       continue
-      //     }
-
-      //   } else if (skipValue !== null && skipCounter < skipValue) {
-      //     continue
-      //   }
-
-      //   if (takeValue !== null && takeCounter >= takeValue) {
-      //     break
-      //   }
-
+      //
+      //     if (!canTakeCursor) continue
+      //
+      //   } else if (skipValue !== null && skipCounter < skipValue) continue
+      //
+      //   if (takeValue !== null && takeCounter >= takeValue) break
+      //
       //   dataLimited.push(data)
       //   takeCounter++
       // }
-      return
+      // return
 
       const result = await this.todoFinder.execute(null)
       // const todos = result.unwrap()
@@ -188,7 +163,7 @@ export class TryCommand extends CommandRunner {
     }
   }
 
-  testList<T>(list: T[], uniqueAttribute: keyof T): T[] {
+  distinctListSet<T>(list: T[], uniqueAttribute: keyof T): T[] {
     const uniqueValues = new Set<T[typeof uniqueAttribute]>()
     const distinctList: T[] = []
 
@@ -203,6 +178,40 @@ export class TryCommand extends CommandRunner {
 
     return distinctList
   }
+
+
+  whereList(todos: Todo[], filters: Filter[]): Todo[] {
+
+    const filteredTodos: Todo[] = []
+    const refinedFilters: Filter[] = []
+    const todo1 = todos[0]
+
+    for (const filter of filters) {
+      if (!todo1.hasOwnProperty(filter.field.value)) break
+      refinedFilters.push(filter)
+    }
+
+    for (const todo of todos) {
+      let match = false
+      for (const filter of refinedFilters) {
+        const filterField = filter.field.value
+        const filterOperator = filter.operator.value
+        const filterValue = filter.value.value
+
+        if (filterOperator === 'equals') {
+          if (todo[filterField].value !== filterValue) break
+        }
+
+        match = true
+      }
+
+      if (match)
+        filteredTodos.push(todo)
+    }
+
+    return filteredTodos
+  }
+
 
   distinctList<T>(list: T[], uniqueAttribute: string): T[] {
     const uniqueValues = new Set<string>()
